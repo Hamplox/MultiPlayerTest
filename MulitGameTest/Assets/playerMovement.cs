e9595489c0011f236af366a33cc85ce2a32eecdf
Hamplox <zorkan.p@gmail.com>
2019-01-26 12:48:51 +0100

    Things

---
+++ MulitGameTest/Assets/playerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour {
+
+    public float speed;
+    public float rotSpeed;
+
+	// Update is called once per frame
+	void Update () {
+
+        float z = Input.GetAxisRaw("Vertical") * speed;
+        float y = Input.GetAxisRaw("Horizontal") * rotSpeed;
+
+        transform.Translate(0, 0, z);
+        transform.Rotate(0, y, 0);
+   
+	}
+}
