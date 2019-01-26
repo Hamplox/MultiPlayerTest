cd9b0953eb30abb98013a3b9de9fb3390a818bcf
Per Eriksson <Pereriksson98@hotmail.com>
2019-01-26 12:40:25 +0100

    networkingtest1

---
+++ MulitGameTest/Assets/Scripts/SetupLocalPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SetupLocalPlayer : NetworkBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+        if (isLocalPlayer)
+        {
+            GetComponent<Movement>().enabled = true;
+            GetComponentInChildren<Camera>().enabled = true;
+        }
+	}
+	
+}
